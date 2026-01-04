878fb44603b532e199b3ff056c2d0a5709bc7e52
imandi0530 <imandi.20240754@iit.ac.lk>
2026-01-04 16:55:10 +0530

    Final coursework updates

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/estate-agent-app/',   // 👈 repo name
+  base: '/estate-agent-app/', // MUST match repo name
   plugins: [react()],
   test: {
     environment: 'jsdom',
